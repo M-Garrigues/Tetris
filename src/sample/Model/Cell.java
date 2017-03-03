a35704752ca2c97755d7dcec5285f72c2e7dbced
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-03-03 19:21:58 +0100

    Cell added. It's designed for one piece per cell only, maybe we'll have to change that.

---
+++ src/sample/Model/Cell.java
@@ -0,0 +1,37 @@
+package sample.Model;
+
+/**
+ * Created by Mathieu on 03/03/2017.
+ */
+public class Cell {
+
+    private Piece piece;
+
+    public Cell (){
+        this.piece = null;
+    }
+
+    public Cell (Piece piece){
+        this.piece = piece;
+    }
+
+
+
+    public void takePiece(){
+        this.piece = null;
+    }
+
+    public void putPiece(Piece piece){
+
+        if(this.piece != null){
+            System.out.println("Current cell was already taken by another piece. Problems may result.");
+        }
+
+        this.piece = piece;
+    }
+
+
+    public boolean isEmpty(){
+        return this.piece == null;
+    }
+}
---
+++ src/sample/Model/Grid.java
@@ -0,0 +1,8 @@
+package sample.Model;
+
+/**
+ * Created by Mathieu on 03/03/2017.
+ */
+public class Grid {
+
+}

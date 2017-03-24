23d1793067a1568abb51d85eae4bc48152d92664
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-03-24 14:55:09 +0100

    Warning lines

--- src/sample/observer/Observable.java
+++ src/sample/observer/Observable.java
@@ -5,6 +5,10 @@ import sample.boardGameLib.model.Piece;
 /**
  * Coded by Mathieu GARRIGUES on 24/03/2017.
  */
+
+// These Observer/Observable interfaces are quite particular to board games, don't use anywhere else as is.
+
+
 public interface Observable {
 
     public void addObserver(Observer obs);
--- src/sample/observer/Observer.java
+++ src/sample/observer/Observer.java
@@ -6,6 +6,8 @@ import sample.boardGameLib.model.Piece;
  * Coded by Mathieu GARRIGUES on 24/03/2017.
  */
 
+// These Observer/Observable interfaces are quite particular to board games, don't use anywhere else as is.
+
 public interface Observer {
     public void updatePiece(Piece piece);
     public void update();

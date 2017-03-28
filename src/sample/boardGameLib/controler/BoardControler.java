3ce4b85eb0291ee1d1d8aa6346ac6d2e8d399006
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-03-28 18:37:26 +0200

    Forgot 2 files

--- src/sample/boardGameLib/controler/BoardControler.java
+++ src/sample/boardGameLib/controler/BoardControler.java
@@ -1,7 +1,12 @@
 package sample.boardGameLib.controler;
 
+import sample.boardGameLib.model.Grid;
+
 /**
  * Coded by Mathieu GARRIGUES on 28/03/2017.
  */
-public class BoardControler {
+public class BoardControler extends AbstractBoardControler {
+
+    Grid board;
+
 }
--- src/sample/tetris/pieces/PieceGenerator.java
+++ src/sample/tetris/pieces/PieceGenerator.java
@@ -1,7 +1,59 @@
 package sample.tetris.pieces;
 
+import sample.boardGameLib.model.Piece;
+
 /**
- * Coded by Mathieu GARRIGUES on 28/03/2017.
+ * Coded by Mathieu GARRIGUES on 26/03/2017.
  */
 public class PieceGenerator {
+
+    public static TetrisPiece randomPiece(){
+        int i = (int)(Math.random() * 7);
+        TetrisPiece piece;
+        switch (i) {
+
+            case 0:
+                piece = new PieceI();
+                break;
+
+            case 1:
+                piece = new PieceL();
+                break;
+
+            case 2:
+                piece = new PieceLI();
+                break;
+
+            case 3:
+                piece = new PieceO();
+                break;
+
+            case 4:
+                piece = new PieceS();
+                break;
+
+            case 5:
+                piece = new PieceT();
+                break;
+
+            case 6:
+                piece = new PieceZ();
+                break;
+
+            default:
+                piece = new PieceI();
+        }
+        return piece;
+    }
+
+    public static TetrisPiece nextRandPiece(Piece piece){
+        int i = (int)(Math.random() * 7);
+        TetrisPiece newPiece;
+
+        newPiece = randomPiece();
+
+        if(newPiece.getClass() == piece.getClass()){newPiece = nextRandPiece(piece);} //if the piece is the same as the previous one, new rand gen
+
+        return newPiece;
+    }
 }
